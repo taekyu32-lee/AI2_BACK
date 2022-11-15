3d44373eba3dfae13da68f67be60e1ec340605cb
taekyu32-lee <taekyu32@gmail.com>
2022-11-15 09:55:39 +0900

    first commit

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Create your tests here.
+class UserRegistrationAPIViewTestCase(APITestCase):
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username":"testuser",
+            "fullname":"테스터",
+            "email":"test@testuser.com",
+            "password":"password",
+        }
+        response = self.client.post(url,user_data)
+        print(response.data)
+        self.assertEqual(response.data['message'],'가입 완료!!')
+
